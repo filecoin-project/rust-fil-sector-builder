d78e9faa39fa74dd24640333bb9a7fd5e1bf5828
laser <l@s3r.me>
2019-06-13 14:00:36 -0700

    feat(drop FFI-nonspecific ops from sector-builder-ffi crate)

--- sector-builder-ffi/src/lib.rs
+++ sector-builder-ffi/src/lib.rs
@@ -2,16 +2,13 @@
 
 // These need to be here because of cbindgen: https://github.com/eqrion/cbindgen/issues/292
 extern crate filecoin_proofs;
+extern crate filecoin_proofs_ffi;
 extern crate sector_builder;
 
 #[macro_use]
 extern crate lazy_static;
-#[macro_use]
-extern crate failure;
 
-mod error;
 mod responses;
 mod singletons;
 
-pub mod builder;
-pub mod stateless;
+pub mod api;

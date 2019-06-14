05af63bc51eb17c1c92e5254f474c98bb0aa12bf
laser <l@s3r.me>
2019-06-13 17:54:35 -0700

    feat(replace Makefile with build script)

---
+++ scripts/build-release.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+build_output_tmp=$(mktemp)
+
+# clean up temp file on exit
+#
+trap '{ rm -f $build_output_tmp; }' EXIT
+
+# build with RUSTFLAGS configured to output linker flags for native libs
+#
+RUSTFLAGS='--print native-static-libs' \
+    cargo +$1 build \
+    --release ${@:2} 2>&1 | tee ${build_output_tmp}
+
+# parse build output for linker flags
+#
+linker_flags=$(cat ${build_output_tmp} \
+    | grep native-static-libs\: \
+    | head -n 1 \
+    | cut -d ':' -f 3)
+
+# generate pkg-config
+#
+sed -e "s;@VERSION@;$(git rev-parse HEAD);" \
+    -e "s;@PRIVATE_LIBS@;${linker_flags};" sector_builder_ffi.pc.template > sector_builder_ffi.pc
+
+# ensure header file was built
+#
+find . -type f -name sector_builder_ffi.h | read
+
+# ensure the archive file was built
+#
+find . -type f -name libsector_builder_ffi.a | read

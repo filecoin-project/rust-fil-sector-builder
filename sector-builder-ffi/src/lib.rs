eb07f2e9ed6076a0b3a396f64703a90b834b888f
Volker Mische <volker.mische@gmail.com>
2019-08-14 18:22:59 +0200

    chore: upgrade to newest cbindgen release (#30)
    * chore: upgrade to newest cbindgen release
    
    Closes #28.
    
    * fix: remove no longer used imports
    
    cbindgen issue https://github.com/eqrion/cbindgen/issues/292 is fixed,
    hence no extra imports are needed anymore.

--- sector-builder-ffi/src/lib.rs
+++ sector-builder-ffi/src/lib.rs
@@ -1,10 +1,5 @@
 #![deny(clippy::all, clippy::perf, clippy::correctness)]
 
-// These need to be here because of cbindgen: https://github.com/eqrion/cbindgen/issues/292
-extern crate filecoin_proofs;
-extern crate filecoin_proofs_ffi;
-extern crate sector_builder;
-
 #[macro_use]
 extern crate log;
 

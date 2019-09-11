51c64008779b28e0d9b39f0a559a536263fd7793
Erin Swenson-Healey <l@s3r.me>
2019-09-11 11:25:35 -0700

    ci(package paramcache instead of paramfetch) (#50)

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -20,7 +20,7 @@ find . -type f -name libsector_builder_ffi.a -exec cp -- "{}" $TAR_PATH/lib/ \;
 find . -type f -name sector_builder_ffi.pc -exec cp -- "{}" $TAR_PATH/lib/pkgconfig/ \;
 
 cargo install filecoin-proofs \
-  --bin paramfetch \
+  --bin paramcache \
   --force \
   --git=https://github.com/filecoin-project/rust-fil-proofs.git \
   --branch=master \
